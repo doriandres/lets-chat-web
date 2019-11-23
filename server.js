f0e54237e79c319d0fb3143b8e9d25a58f960813
Dorian Andres <>
2019-11-22 22:42:52 -0600

    se quito body parser

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
