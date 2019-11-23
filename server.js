b49db5d3cae79b8e9d6ca158ef3ba0d60fb82d44
Dorian Andres <>
2019-11-22 23:59:40 -0600

    status

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = express();
 
+app.use(require('express-status-monitor')());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
